939261d96863078e608299212275ca73f2137c25
Maximilian Rudolph <rudolph-max@gmx.net>
2021-11-09 22:33:27 +0100

    Update forshare.js - Adding logo-imgs to sharebuttons

--- assets/scripts/forshare.js
+++ assets/scripts/forshare.js
@@ -1,4 +1,20 @@
-console.log("test");
+let services = JSON.parse( '{ "twitter": { "name": "twitter", "img-src": "twitter.svg"}, "linkedin": "nope" }' );
+
 let data = JSON.parse(forShareData);
 
-Array.from( document.getElementsByClassName('fs-sharebutton') );
+Array.from( document.getElementsByClassName('fs-sharebutton') ).forEach( (e) => {
+  if (!e.classList.contains('fs-exclude') && e.tagName == 'DIV') { // excluding developer-buttons
+
+    let fs_img = document.createElement('img');
+    fs_img.setAttribute('src', 'assets/media/logos/' + services['twitter']['img-src']);
+    fs_img.classList.add("fs-sharebutton-logo");
+    fs_img.setAttribute('alt', '');
+    fs_img.setAttribute('role', 'presentation');
+
+    e.appendChild( fs_img );
+
+
+    console.log(data.selectedServices, services['twitter']['img-src']);
+
+  }
+});
