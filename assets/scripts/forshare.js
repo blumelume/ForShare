7b3250d7db5f3d53062b16cfdeb32d3a4532cb9c
Maximilian Rudolph <rudolph-max@gmx.net>
2021-11-10 15:42:18 +0100

    Update forshare.js

--- assets/scripts/forshare.js
+++ assets/scripts/forshare.js
@@ -1,20 +1,19 @@
-let services = JSON.parse( '{ "twitter": { "name": "twitter", "img-src": "twitter.svg"}, "linkedin": "nope" }' );
+let services = JSON.parse( '{"services": [ {"name": "twitter", "img-src": "twitter.svg"}, {"name": "linked", "img-src": "linkedin.svg"} ]}' );
 
 let data = JSON.parse(forShareData);
 
+let i = 0;
 Array.from( document.getElementsByClassName('fs-sharebutton') ).forEach( (e) => {
   if (!e.classList.contains('fs-exclude') && e.tagName == 'DIV') { // excluding developer-buttons
 
+    // console.log(i, services['services'][i]['img-src']);
     let fs_img = document.createElement('img');
-    fs_img.setAttribute('src', 'assets/media/logos/' + services['twitter']['img-src']);
+    fs_img.setAttribute('src', 'assets/media/logos/' + services['services'][i]['img-src']);
     fs_img.classList.add("fs-sharebutton-logo");
     fs_img.setAttribute('alt', '');
     fs_img.setAttribute('role', 'presentation');
 
     e.appendChild( fs_img );
-
-
-    console.log(data.selectedServices, services['twitter']['img-src']);
-
+    i++;
   }
 });
